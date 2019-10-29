a4419da421037b56769452bebc6f99fe5086046e
vignesh <vignesh.vellingiri@aot-technologies.com>
2019-10-29 16:06:08 -0700

    code cleanup

--- FormControlApi/src/main/java/com/aot/forms/repository/OrbeonMetaDataRepositoryCustom.java
+++
@@ -1,9 +0,0 @@
-package com.aot.forms.repository;
-
-import java.util.List;
-
-import com.aot.forms.formApi.OrbeonMetaData;
-
-public interface OrbeonMetaDataRepositoryCustom {
-	  List<OrbeonMetaData> findByCamundaId(String camunda_id);
-}
